16e05b55bdbf744fab7ee0e59e7148d0d455ea67
Mike Merrill <mike.w.merrill@gmail.com>
2012-03-01 22:36:42 -0500

    just playing around with git

---
+++ ReverseWords/src/ReverseWords.java
@@ -0,0 +1,36 @@
+public class ReverseWords {
+
+	public static String reverse(String sentence) {
+		StringBuilder reversed = new StringBuilder(sentence.length());
+		String[] words = sentence.split("\\s+");
+		for (int i = 0; i < words.length; i++) {
+			reversed.append(words[words.length - 1 -i]);
+			if (i + 1 < words.length) {
+				reversed.append(' ');
+			}
+		}
+		return reversed.toString();
+	}
+	
+	public static String[] reverse(String[] sentence) {
+		String[] reversed = new String[sentence.length];
+		for (int i = 0; i < sentence.length; i++) {
+			reversed[i] = sentence[sentence.length - 1 - i];
+		}
+		return reversed;
+	}
+	
+	public static void print(String[] sentence) {
+		for (String word : sentence) {
+			System.out.print(word + ' ');
+		}
+		System.out.println();
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		print(args);
+		print(reverse(args));
+		System.out.println(reverse("The    cat is out of the bag"));
+	}
+}
